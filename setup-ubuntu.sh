c42e3d80f50c6b0c905c1ab607dadb650c1f3929
Muhamad Fadhlan <158328676+mfadhlan24@users.noreply.github.com>
2024-07-01 02:08:46 +0700

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -65,8 +65,14 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 # Load nvm into the current shell session
 echo "Loading nvm into the current shell session..."
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Ini akan me-load nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Ini akan me-load nvm bash_completion
+
+# Add nvm load script to .bashrc
+echo "Adding nvm load script to .bashrc..."
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.bashrc
 
 # Install the stable version of Node.js
 echo "Installing the stable version of Node.js..."
